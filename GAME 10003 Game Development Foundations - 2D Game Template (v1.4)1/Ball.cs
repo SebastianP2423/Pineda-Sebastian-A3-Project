d2a124822fdf7c64cfd45538fb40a3ad7f6e97f6
SebastianP2423 <000946282@mohawkcollege.ca>
2025-11-12 14:46:46 -0500

    ball and paddle classes have been specified

---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Ball.cs
@@ -0,0 +1,35 @@
+﻿using MohawkGame2D;
+using System.Numerics;
+
+public class Ball
+    {
+    public Vector2 position;
+    public Vector2 size;
+    public Vector2 velocity = new Vector2(4, 2);
+    public Color color = Color.White;
+
+    public Ball(Vector2 pos, Vector2 sz)
+    {
+        position = pos;
+        size = sz;
+    }
+
+    public void Move()
+    {
+        position += velocity;
+    }
+
+    public void ChangeColor()
+    {
+        color = Random.Color();
+    }
+
+    public void Render()
+    {
+        Draw.FillColor = color;
+        Draw.LineColor = Color.Black;
+        Draw.Rectangle(position, size);
+    }
+}
+    
+
---
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using MohawkGame2D;
+using System.Numerics;
+
+public class Paddle
+    {
+    public Vector2 position;
+    public Vector2 size;
+    public Color color;
+
+    public Paddle(Vector2 pos, Vector2 sz, Color col)
+    {
+        position = pos;
+        size = sz;
+        color = col;
+    }
+
+    public void Render()
+    {
+        Draw.FillColor = color;
+        Draw.LineColor = Color.Black;
+        Draw.Rectangle(position, size);
+    }
+}
